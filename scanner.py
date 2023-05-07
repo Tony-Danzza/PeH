04a6e2c708bd03b4a2212155af5e54a3ea2b8bd1
TT <tonydanzza0991@gmail.com>
2023-05-06 20:07:17 -0700

    bad scanner cont.

--- scanner.py
+++ scanner.py
@@ -18,4 +18,27 @@ else:
 print('-' * 50)
 print('Scanning Target' + target)
 print("Time Started:" + str(datetime.now()))
+print('-' * 50)
 
+try:
+    for port in range(50,85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex(target, port) #returns an error indicator
+        if result == 0:
+            print(f"Port {port} is open")
+        
+        s.close()
+
+except KeyboardInterrupt:
+    print("Scan interrupted")
+    print("Exiting Program")
+    sys.exit()
+
+except socket.gaierror:
+    print("Hostname was not resolved.")
+    sys.exit()
+
+except socket.error:
+    print("Couldn't connect to server.")
+    sys.exit()
