ccfb8166ecfe7fe6d7e1e54ab1502af96d17ead2
TT <tonydanzza0991@gmail.com>
2023-05-06 19:58:23 -0700

    bad scanner

--- scanner.py
+++ scanner.py
@@ -2,8 +2,20 @@
 import sys
 import socket
 from datetime import datetime
+# print("hello git test")
 
 #Define Target
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1]) #translate host name to ipv4
+else:
+    print("Invalid amount of arguments")
+    print("Syntax: python3 scanner.py <ip>")
 
-print("hello git test")
 # python3 scanner.py <ip>
+
+#add a pretty banner
+
+print('-' * 50)
+print('Scanning Target' + target)
+print("Time Started:" + str(datetime.now()))
+
